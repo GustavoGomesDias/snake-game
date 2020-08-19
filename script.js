07d7a8f983853c043bb3c30c02e98fecfb0b4fbc
GustavoGomesDias <treeelevengg@gmail.com>
2020-08-18 23:06:53 -0300

    Snake criada

--- script.js
+++ script.js
@@ -6,6 +6,14 @@ let context = canvas.getContext("2d");
 
 let box = 32;
 
+let snake = [];
+
+// Cria a cobrinha
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
+
 function criarBG(){
     /* Criar background */
 
@@ -16,4 +24,16 @@ function criarBG(){
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
+function createSnake(){
+    for(i = 0; i < snake.length; i++){
+        context.fillStyle = "green";
+
+        /* Aqui ela vai ter o tamanho de um quadradinho e vai ander por x e y, com relação a iteração. Ou seja, i igual a 1, a cobra vai tá em x = 1 e y = 1*/
+        context. fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
 criarBG();
+createSnake();
+
+/* Cobra é um array de coordenadas. */
